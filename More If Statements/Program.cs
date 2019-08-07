546d9e16c6a7e98a697bcf193cd668d61030c3e4
oliviaalchemy <olivia@alchemytechs.com>
2019-08-07 10:12:19 +0100

    Add project files.

---
+++ More If Statements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace More_If_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(20, 10, 4));
+
+            Console.ReadLine();
+
+        }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result; 
+            if (num1 >= num2 && num1 >= num3) 
+            {
+                result = num1;
+            } else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            } 
+            else
+            {
+                result = num3;
+            }
+
+            return result; 
+        }
+    }
+}
